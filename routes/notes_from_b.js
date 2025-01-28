45c62333a131f6c869891f1fd50e5367526309a1
MarudeMadao <okamoto100001@gmail.com>
2025-01-29 00:43:59 +0900

    add api app test

---
+++ routes/notes_from_b.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+const cors = require('cors'); //corsミドルウェアの追加
+
+router.use(cors());
+
+
+ // 接続情報を設定
+ const { MongoClient } = require("mongodb");
+ const uri = "mongodb+srv://2201100307fi:uai6HrV7Dn2RBcWR@test.xbaz7.mongodb.net/?retryWrites=true&w=majority&appName=test";
+ const client = new MongoClient(uri);
+
+ router.get('/', async (req, res) => {
+ // データベース、コレクションを指定
+const database = client.db('notes');
+ const notes = database.collection('notes');
+
+ // idが１のドキュメントを取得
+const note = await notes.find({}).toArray();
+ res.json(note);
+ })
+
+ module.exports = router;
