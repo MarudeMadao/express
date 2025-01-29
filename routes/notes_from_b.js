dcbe9f316b9ba57a74155e56f9dc4cc3c0a15b7f
MarudeMadao <okamoto100001@gmail.com>
2025-01-29 16:34:30 +0900

    add .env for DB

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,24 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
 
-const cors = require('cors'); //corsミドルウェアの追加
-
-router.use(cors());
+router.use(cors()); // corsミドルウェアを使用
 
 
- // 接続情報を設定
- const { MongoClient } = require("mongodb");
- const uri = "mongodb+srv://22";
- const client = new MongoClient(uri);
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
 
- router.get('/', async (req, res) => {
- // データベース、コレクションを指定
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
 const database = client.db('notes');
- const notes = database.collection('notes');
+const notes = database.collection('notes');
 
- // idが１のドキュメントを取得
+// idが１のドキュメントを取得
 const note = await notes.find({}).toArray();
- res.json(note);
- })
+res.json(note);
+})
 
- module.exports = router;
+module.exports = router;
